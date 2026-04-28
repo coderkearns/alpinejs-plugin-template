75f674ad9cdba0e702a9f902289598c36556c6d3
coderkearns <coder.kearns@gmail.com>
2026-04-28 10:22:46 -0600

    Add JSDoc comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+/**
+ * @typedef {Object} PluginOptions
+ * @property {string} [name] - The name to say hi to in `x-say-hi` instances. Defaults to "World".
+ */
+
+/**
+ * Alpine plugin providing hello world functionality.
+ * Provides one directive: `x-say-hi`
+ * @param {PluginOptions} [options]
+ * @returns {(Alpine: import('alpinejs').Alpine) => void}
+ */
 export default function templatePlugin(options = { name: "World" }) {
     return (Alpine) => {
         Alpine.directive("say-hi", (el, { expression }, { evaluate }) => {
